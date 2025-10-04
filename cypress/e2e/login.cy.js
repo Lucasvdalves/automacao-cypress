6c2f3ce03f8c807a5fefebdcd89b8481f0d77f4f
Damasceno <lucasalvesbh2018@gmail.com>
2025-10-04 16:48:18 -0300

    feat - teste de login na aplicacao soucedemo com usuario incorreto

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,6 +8,19 @@ describe('Login', () => {
         cy.get('[data-test="login-button"]').click()
 
         //Assert
-        cy.url().should('include', '/inventory.html')
+        cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
+    })
+
+    it.only('Realizar login com usuário inválido', () => {
+        //Arrange
+        cy.visit('https://www.saucedemo.com/')
+        //Act
+        cy.get('[data-test="username"]').type('user.invalid')
+        cy.get('[data-test="password"]').type('senha')
+        cy.get('[data-test="login-button"]').click()
+
+        //Assert
+        cy.get('[data-test="error"]').should('be.visible')
+        cy.url().should('eq', 'https://www.saucedemo.com/')
     })
 })
